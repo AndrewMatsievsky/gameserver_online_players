c81d6ade2ee8c5fa84a936164b9de8c3d150adb4
AndrewMatsievsky <strymok89@gmail.com>
2016-11-09 18:28:23 +0200

    Removed unneccessary loop

--- t.rb
+++ t.rb
@@ -1,6 +1,3 @@
 output = `curl https://unturned-servers.net/server/91430/`
-
-100.times do
-  sleep 600
-  exec("notify-send 'Server players:' '#{output.scan(/\d \/ 24/).first}'; ruby t.rb")
-end
+sleep 15
+exec("notify-send 'Server players:' '#{output.scan(/\d \/ 24/).first}'; ruby t.rb")
